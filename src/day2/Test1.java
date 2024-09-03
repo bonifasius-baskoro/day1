396ee03e4a6b8d0fd9ffcb191849fda86a32e029
Jerry Baskoro <bjbbeat@gmail.com>
2024-09-03 11:02:19 +0700

    add some getter

--- src/day2/Test1.java
+++ src/day2/Test1.java
@@ -1,5 +1,14 @@
 package day2;
 
 public class Test1 {
+    int age;
+    int money;
 
+    public int getAge() {
+        return age;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
